b7434c983fdb56b2a4a2ecb5e9593b9a69012a50
sam gee <48070584+sam-gee@users.noreply.github.com>
2021-01-03 22:46:16 -0800

    Add files via upload
    2021 CS50 problem, set 1, normal

---
+++ mario-less.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height of the pyramid: \n");
+    }
+    while ((height < 1) || (height > 8));
+
+
+    for (int n = height; n > 0 ; n--)
+    {
+        //indentation
+        for (int j = 0 ; j < n - 1 ; j++)
+        {
+            printf(" ");
+        }
+        //left pyramid
+        for (int k = n - 1; k < height ; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
+
